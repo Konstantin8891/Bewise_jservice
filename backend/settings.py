14c423ebcead7835d5a866de489522d72df5aeca
Konstantin8891 <89670253660@mail.ru>
2023-05-18 13:20:20 +0300

    Fix dotenv bug

--- backend/settings.py
+++ backend/settings.py
@@ -4,10 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-RECORDS_DIR = os.path.join(BASE_DIR, 'records')
-
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
