f4dddaa034f571e42b8783fd06a3089edc2c524d
Konstantin8891 <89670253660@mail.ru>
2023-05-18 13:23:02 +0300

    Fix dotenv

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,9 @@
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from settings import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, HOST
 
-
-load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = (
     f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{HOST}/{POSTGRES_DB}'
